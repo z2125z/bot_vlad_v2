0efeb218e6c25a4c517b6e5707239352d8309b52
z2125z <117429299+z2125z@users.noreply.github.com>
2025-10-23 21:50:46 +0500

    fix: исправлены импорты в admin_handlers/__init__.py, удалены некорректные user_handlers импорты

--- handlers/admin_handlers/__init__.py
+++ handlers/admin_handlers/__init__.py
@@ -1,17 +1,13 @@
-from .user_handlers.user import router as user_router
-from .user_handlers.debug import router as debug_router
-from .admin_handlers.main_menu import router as admin_main_router
-from .admin_handlers.admin import router as admin_router
-from .admin_handlers.statistics import router as stats_router
-from .admin_handlers.excel_reports import router as excel_reports_router
-from .admin_handlers.mailing_creator import router as mailing_creator_router
-from .admin_handlers.templates_manager import router as templates_router
-from .admin_handlers.mailing_history import router as mailing_history_router
-from .admin_handlers.user_mailing import router as user_mailing_router
+from .main_menu import router as admin_main_router
+from .admin import router as admin_router
+from .statistics import router as stats_router
+from .excel_reports import router as excel_reports_router
+from .mailing_creator import router as mailing_creator_router
+from .templates_manager import router as templates_router
+from .mailing_history import router as mailing_history_router
+from .user_mailing import router as user_mailing_router
 
 all_routers = [
-    user_router,
-    debug_router,
     admin_main_router,
     admin_router,
     stats_router,
